62bf8ae679fc8917ce040cf3425776576f402250
ehfhcrc <ehenrich@fredhutch.org>
2017-12-18 14:45:01 -0800

    abstracted metaData functionality

--- data-raw/rdaMaker.R
+++ data-raw/rdaMaker.R
@@ -7,13 +7,16 @@ bsdChk <- read.csv("data-raw/basicStudyDesign.csv", stringsAsFactors = FALSE)
 save(bsdChk, file = "data/bsdChk.rda")
 
 # Protocols
-
+protocolsChk <- read.csv("data-raw/protocols.csv", stringsAsFactors = FALSE)
+save(protocolsChk, file = "data/protocolsChk.rda")
 
 # Treatments
-
+treatmentsChk <- read.csv("data-raw/treatments.csv", stringsAsFactors = FALSE)
+save(treatmentsChk, file = "data/treatmentsChk.rda")
 
 # SubjectsHuman
-
+subjectsHumanChk <- read.csv("data-raw/subjectsHuman.csv", stringsAsFactors = FALSE)
+save(subjectsHumanChk, file = "data/subjectsHumanChk.rda")
 
 ###########################################
 ###            Assay-Data               ###
