fa5cfacc2f7e1dd44791e16d841766c682c6286e
ehfhcrc <ehenrich@fredhutch.org>
2018-03-06 14:15:34 -0800

    add getTemplateNames

--- R/geo2immport.R
+++ R/geo2immport.R
@@ -2,21 +2,17 @@
 #'
 #' @description Given a GEO Series accession, geo2ImmPort will output a
 #'
-#' @param geoGSE string, GEO Series accession
+#' @param softGzPath string, GEO .soft.gz local filepath
 #' @param outputDir string, filepath for output. default is NULL.
 #' @param stdNorm boolean, standardize and normalize exprMx.
 #' @importFrom GEOquery getGEO getGEOfile
 #' @importFrom preprocessCore normalize.quantiles
+#' @import ImmuneSpaceR
 #' @export
-geo2ImmPort <- function(geoGSE, outputDir = NULL, stdNorm = TRUE){
+geo2ImmPort <- function(softGzPath, outputDir = NULL, stdNorm = TRUE){
 
-    # get GSE SOFT file
-    tmpDir <- tempdir()
-    dmp <- getGEOfile(geoGSE, destdir = tmpDir)
-    files <- list.files(tmpDir)
-    file <- files[ grep(geoGSE, files) ]
-
-    tmp <- getGEO(filename = file)
+    # get GSE class from .soft.gz local file
+    tmp <- getGEO(filename = softGzPath)
 
     # transform GSE class results into dataFrame
     tmpTbls <- lapply(names(tmp@gsms), function(x){
@@ -25,6 +21,8 @@ geo2ImmPort <- function(geoGSE, outputDir = NULL, stdNorm = TRUE){
         return(tbl)
     })
     exprs <- Reduce(f = function(x,y){ merge(x,y, by = "ID_REF", all = T)}, tmpTbls)
+    rownames(exprs) <- exprs$ID_REF
+    exprs <- exprs[,-1] # rm ID_REF
 
     # Taken from LabKeyModules/HIPCMatrix/pipeline/tasks/CreateMatrix.R fn - .microArrayPipe()
     if(stdNorm == TRUE){
@@ -37,24 +35,14 @@ geo2ImmPort <- function(geoGSE, outputDir = NULL, stdNorm = TRUE){
         if( max(exprs) > 100 ){ exprs <- log2(exprs) }
     }
 
-    # Create ImmPort experimentSamples.gene_expression_array template from metaData
-    # meta <- getTemplateDF("experimentSamples.Gene_Expression_Array")
-
     # write to outputFile
     if(!is.null(outputDir)){
         write.table(exprs,
                     file = paste0(outputDir, "/", geoGSE, "_exprs.txt"),
                     sep = "\t",
                     quote = FALSE,
-                    row.names = FALSE)
-
-        # write.table(meta,
-        #             file = paste0(outputDir, "/", geoGSE, "_expSmpls.txt"),
-        #             sep = "\t",
-        #             quote = FALSE,
-        #             row.names = FALSE)
+                    row.names = TRUE)
     }
 
-    # return(list(exprs = exprs, expSmpls = meta))
     return(exprs)
 }
---
+++ R/getTemplateNames.R
@@ -0,0 +1,9 @@
+# ---- MAIN FN --------------------------------------------------------
+#' @title get names of templates that can be retrieved via getTemplateDF
+#'
+#' @description returns vector of possible templates
+#'
+#' @export
+getTemplateNames <- function(){
+    return( unique(ImmPortTemplates$templateName) )
+}
